fb773b81252c9f840355fae2c8a2ae26bdb34f3e
Daniel Longacre <danieljlongacre@gmail.com>
2023-10-04 12:52:20 -0400

    Added files seed, .env, .gitignore, server. Added dependencies. Started server and config files.

---
+++ server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const path = require('path');
+const express = require('express');
+const routes = require('./controllers');
+
+const sequelize = require('./config/connection');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
+  });
