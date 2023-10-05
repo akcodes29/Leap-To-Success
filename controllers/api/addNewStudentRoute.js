dd62fbc7a9ac2bc90b60455a5913812a814f6d1d
Andrea <keriazes.andrea@gmail.com>
2023-10-05 19:35:22 -0400

    added api routes for addnewstudent

---
+++ controllers/api/addNewStudentRoute.js
@@ -0,0 +1,41 @@
+const router = require('express').Router();
+const Student = require('../../models/Student');
+
+
+
+// Try this for getting all students
+router.get('/', (req, res) => {
+    // Get all students from the student table
+    Student.findAll().then((studentData) => {
+      res.json(studentData);
+    });
+ });
+
+
+// Try this for getting all students
+  router.get('/', async (req, res) => {
+    try {
+      // Get all students and JOIN with student data
+      const studentData = await Student.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ['name'],
+          },
+        ],
+      });
+
+      const students = studentData.map((student) => student.get({ plain: true }));
+
+      // Pass serialized data and session flag into template
+      res.render('homepage', { 
+        students, 
+        logged_in: req.session.logged_in 
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+
+  module.exports = router;
