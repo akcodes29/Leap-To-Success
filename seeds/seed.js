c71924b9a218b70443b5d892dfc5934dc81eca82
Daniel Longacre <danieljlongacre@gmail.com>
2023-10-05 18:58:21 -0400

    Finished seed.js file to seed the database

--- seeds/seed.js
+++ seeds/seed.js
@@ -3,3 +3,25 @@ require('dotenv').config({
 });
 
 const sequelize = require('../config/connection');
+const { Student, Teacher } = require('../models');
+
+const studentData = require('./studentData.json');
+const teacherData = require('./teacherData.json');
+
+const seedDatabase = async() => {
+    await sequelize.sync({ force: true});
+
+    const students = await Student.bulkCreate(studentData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    const teachers = await Teacher.bulkCreate(teacherData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    process.exit(0);
+};
+
+seedDatabase();
