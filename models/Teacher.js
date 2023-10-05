86ed7c8e2f6f64b8965677e865b07c8bcf927eb1
Daniel Longacre <danieljlongacre@gmail.com>
2023-10-04 21:08:14 -0400

    Added email and password fields, hooks, and checkPassword method to the Teacher Class model

--- models/Teacher.js
+++ models/Teacher.js
@@ -1,7 +1,12 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
-class Teacher extends Model {}
+class Teacher extends Model {
+    checkPassword(loginAttempt) {
+        return bcrypt.compareSync(loginAttempt, this.password);
+    }
+}
 
 Teacher.init(
     {
@@ -19,6 +24,21 @@ Teacher.init(
             type: DataTypes.STRING,
             allowNull: false,
         },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+              isEmail: true,
+            },
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+              len: [8],
+            },
+        },
         student_id: {
             //Foreign Key
             type: DataTypes.INTEGER,
@@ -29,6 +49,16 @@ Teacher.init(
         },
     },
     {
+        hooks: {
+            beforeCreate: async (newUserData) => {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            beforeUpdate: async (updatedUserData) => {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            },
+        },
         sequelize,
         timestamps: false,
         freezeTableName: true,
