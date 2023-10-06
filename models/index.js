bd6aca89cc5f91131478210047ea0c13b6931425
abbyjo <abbyjo225@gmail.com>
2023-10-05 22:41:17 -0400

    Added Goals model relationships to Student and Teacher.

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const Parent = require('./Parent');
 const Student = require('./Student');
 const Teacher = require('./Teacher');
+const Goals = require('./Goals');
 
 //Relationships
 Teacher.hasMany(Student, {
@@ -11,5 +12,20 @@ Teacher.hasMany(Student, {
 Student.belongsTo(Teacher, {
     foreignKey: 'teacher_id'
 });
+
+
+//Goals table relationships -- work in progress 
+Student.hasMany(Goals, {
+    foreignKey: 'student_id',
+    onDelete: 'CASCADE', 
+});
+
+Goals.belongsTo(Teacher , {
+    foreignKey: 'teacher_id'
+});
+
+Goals.belongsTo(Student, {
+    foreignKey: 'student_id'
+})
 
 module.exports = { Parent, Student, Teacher};
