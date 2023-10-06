2e86666e6fa145373a75f3e63a385aef09bca80f
abbyjo <abbyjo225@gmail.com>
2023-10-05 22:03:30 -0400

    Added Goals model

---
+++ models/Goals.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+
+class Goals extends Model {};
+
+Goals.init(
+{
+    id:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    name:{
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    teacher_id:{
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'teacher',
+            key: 'id',
+        },
+    },
+    student_id:{
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'student',
+            key: 'id',
+        },
+    },
+},
+{
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'goal', 
+}
+)
+
+module.exports = { Goals }
