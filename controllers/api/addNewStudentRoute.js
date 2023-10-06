78609dae3887f61771f1b43f3f1d729e8921e44f
Sarah Goodell <sarahlgoodell1@gmail.com>
2023-10-05 20:16:29 -0400

    Created utils folder and withAuth program

--- controllers/api/addNewStudentRoute.js
+++ controllers/api/addNewStudentRoute.js
@@ -37,5 +37,17 @@ router.get('/', (req, res) => {
     }
   });
   
+  routerouter.post('/', withAuth, async (req, res) => {
+    try {
+      const newProject = await Project.create({
+        ...req.body,
+        user_id: req.session.user_id,
+      });
+  
+      res.status(200).json(newProject);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
 
   module.exports = router;
