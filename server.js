c1e1a65d3e4e7a8105c1dd20bf8f86f80fa83138
abbyjo <abbyjo225@gmail.com>
2023-10-07 16:58:17 -0400

    Added session + session store to server file

--- server.js
+++ server.js
@@ -3,11 +3,27 @@ require('dotenv').config();
 const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
+const session = require('express-session');
 
 const sequelize = require('./config/connection');
 
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const app = express();
 const PORT = process.env.PORT;
+
+
+const sess = {
+  secret: 'Super mega ultra secrte secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
