e04095eef0604319b2c71438c6d5d677d2e20df6
Sarah Goodell <sarahlgoodell1@gmail.com>
2023-10-06 22:43:25 -0400

    Created click event for creating new teacher user

--- public/scripts/signup.js
+++ public/scripts/signup.js
@@ -1,13 +1,31 @@
 require("dotenv").config();
 // Packages needed for this application
-const inquirer = require('inquirer');
 const mysql = require('mysql2');
-const connection = require("./db/connection");
+const connection = require("../../config/connection");
+const button = document.getElementById("btn");
 
-const button = document.getElementsByClassName("btn");
+// function runner() {
+//     console.log(`I am running`)
+//     fetch('/api/teacher', {
+//         method: 'POST',
+//         body: new FormData(document.querySelector('form'))
+//     })
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+// };
 
-function runner() {
-    console.log(`I am running`)
-}
+// runner()
 
-runner();
+button.addEventListener("click", function () {
+    console.log('I was clicked')
+})
+
+button.addEventListener('click', function (event) {
+    event.preventDefault();
+    fetch('/api/teacher', {
+        method: 'POST',
+        body: new FormData(document.querySelector('form'))
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+})
