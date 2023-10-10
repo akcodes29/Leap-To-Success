91f55c3eaebd91bf754e74af8c976ab1088723ef
Sarah Goodell <sarahlgoodell1@gmail.com>
2023-10-10 16:24:41 -0400

    Created logout button and logic associated with it

---
+++ public/scripts/logout.js
@@ -0,0 +1,15 @@
+// Create a fetch for the logout button
+const logout = async () => {
+    const response = await fetch('/api/teacher/logout', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to log out.');
+    }
+  };
+  
+  document.querySelector('#logout').addEventListener('click', logout);
