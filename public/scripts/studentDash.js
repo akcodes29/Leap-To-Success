95f86147754f09e2d743875535bbcacd21778589
abbyjo <abbyjo225@gmail.com>
2023-10-11 21:39:24 -0400

    Completed functions for small lily rotation

--- public/scripts/studentDash.js
+++ public/scripts/studentDash.js
@@ -1,6 +1,6 @@
 // Selects necessary elements 
 const bigLilyPad = document.querySelector('#goal');
-const smallLilyPad = document.querySelector('#small-lily');
+const smallLilyPad = document.querySelector('.sImg');
 
 let counter = 1;
 // Function to handle change of large lily pad 
@@ -16,24 +16,36 @@ function leapFrogger() {
         counter = 0;
     };
 };
-
-const happyFace = () => {
-    //smiley face button click event
-    leapFrogger();
+//Functions for small lily pad movement
+function happyFace () {
+    if(counter == 0) {
+        smallLilyPad.src = 'assets/images/lilypads/lilypad G2.png'
+    } else if ( counter == 1) {
+        smallLilyPad.src = 'assets/images/lilypads/lilypad G3.png'
+    } else if (counter == 2) {
+        smallLilyPad.src = 'assets/images/lilypads/lilypad G1.png'  
+    };
 };
 
-const sadFace = () => {
-    //sad face button click event
-    leapFrogger();
+function sadFace() {
+    if(counter == 0) {
+        smallLilyPad.src = 'assets/images/lilypads/lilypad R2.png'
+    } else if ( counter == 1) {
+        smallLilyPad.src = 'assets/images/lilypads/lilypad R3.png'
+    } else if (counter == 2) {
+        smallLilyPad.src = 'assets/images/lilypads/lilypad R1.png'  
+    };
 };
 
 // Event listeners 
 $(document).on('click', '#happy-face',function(event){
     event.preventDefault();
     leapFrogger();
+    happyFace();
 });
 
 $(document).on('click', '#sad-face',function(event){
     event.preventDefault();
     leapFrogger();
+    sadFace();
 });
